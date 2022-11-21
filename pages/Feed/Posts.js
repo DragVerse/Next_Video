92fe6b54fc0f0e282479f43e6b51c2d1c61850f7
ImArnoldDCODES <danielsarnold010@gmail.com>
2022-11-21 13:41:59 -0800

    feed Done

--- pages/Feed/Posts.js
+++ pages/Feed/Posts.js
@@ -2,27 +2,126 @@ import styles from './Feed.module.css'
 import Image from 'next/image'
 import Profile from '../assets/profile2.png'
 import Verified from '../assets/verified.png'
+import Comment from '../assets/comment.png'
+import Transimit from '../assets/transmit.png'
+import Heart from '../assets/heart.png'
+import Brower from '../assets/brower.png'
+import Menu from '../assets/menu.png'
 
 export default function Post() {
     return (
-        <div className={styles.build}>
+        <div className={styles.build2}>
             <div className={styles.Bigbox}>
                 <div className={styles.box}>
                     <div className={styles.post}>
-                    <main>
-                        <div>
-                        <Image src={Profile} alt='profile' width={40} height={40}/>
-                        <section>
-                        <div className={styles.name}>
-                            saltï 
-                            <Image src={Verified} alt='verified check' width={15} height={15} className={styles.verified}/>
+                        <main>
+                            <div>
+                                <Image src={Profile} alt='profile' width={40} height={40} />
+                                <section>
+                                    <div className={styles.name}>
+                                        saltï
+                                        <Image src={Verified} alt='verified check' width={15} height={15} className={styles.verified} />
+                                    </div>
+                                    <div className={styles.username}>@salti.lens</div>
+                                </section>
+                            </div>
+                            <h4>9 minutes ago</h4>
+                        </main>
+                        <div className={styles.text}>
+                            <span>I am so proud of all the hard work that the dragverse team it’s putting into buildin this network - I am a lucky drag queen!💋</span>
+                            <div className={styles.react}>
+                                <li><Image src={Comment} alt='' width={20} height={20} /></li>
+                                <li><Image src={Transimit} alt='' width={20} height={20} /></li>
+                                <li><Image src={Heart} alt='' width={20} height={20} /></li>
+                                <li><Image src={Brower} alt='' width={20} height={20} /></li>
+                                <li><Image src={Menu} alt='' width={20} height={20} /></li>
+                            </div>
                         </div>
-                        <div className={styles.username}>@salti.lens</div>
-                        </section>
-                        </div>
-                        <h4>9 minutes ago</h4>
-                    </main>
 
+                    </div>
+
+                    <div className={styles.post}>
+                        <main>
+                            <div>
+                                <Image src={Profile} alt='profile' width={40} height={40} />
+                                <section>
+                                    <div className={styles.name}>
+                                        saltï
+                                        <Image src={Verified} alt='verified check' width={15} height={15} className={styles.verified} />
+                                    </div>
+                                    <div className={styles.username}>@salti.lens</div>
+                                </section>
+                            </div>
+                            <h4>9 minutes ago</h4>
+                        </main>
+                        <div className={styles.text}>
+                            <span>I am so proud of all the hard work that the dragverse team it’s putting into buildin this network - I am a lucky drag queen!💋</span>
+                            <div className={styles.react}>
+                                <li><Image src={Comment} alt='' width={20} height={20} /></li>
+                                <li><Image src={Transimit} alt='' width={20} height={20} /></li>
+                                <li><Image src={Heart} alt='' width={20} height={20} /></li>
+                                <li><Image src={Brower} alt='' width={20} height={20} /></li>
+                                <li><Image src={Menu} alt='' width={20} height={20} /></li>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div className={styles.post}>
+                        <main>
+                            <div>
+                                <Image src={Profile} alt='profile' width={40} height={40} />
+                                <section>
+                                    <div className={styles.name}>
+                                        saltï
+                                        <Image src={Verified} alt='verified check' width={15} height={15} className={styles.verified} />
+                                    </div>
+                                    <div className={styles.username}>@salti.lens</div>
+                                </section>
+                            </div>
+                            <h4>9 minutes ago</h4>
+                        </main>
+                        <div className={styles.text}>
+                            <span>I am so proud of all the hard work that the dragverse team it’s putting into buildin this network - I am a lucky drag queen!💋</span>
+                            <div className={styles.react}>
+                                <li><Image src={Comment} alt='' width={20} height={20} /></li>
+                                <li><Image src={Transimit} alt='' width={20} height={20} /></li>
+                                <li><Image src={Heart} alt='' width={20} height={20} /></li>
+                                <li><Image src={Brower} alt='' width={20} height={20} /></li>
+                                <li><Image src={Menu} alt='' width={20} height={20} /></li>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div className={styles.post}>
+                        <main>
+                            <div>
+                                <Image src={Profile} alt='profile' width={40} height={40} />
+                                <section>
+                                    <div className={styles.name}>
+                                        saltï
+                                        <Image src={Verified} alt='verified check' width={15} height={15} className={styles.verified} />
+                                    </div>
+                                    <div className={styles.username}>@salti.lens</div>
+                                </section>
+                            </div>
+                            <h4>9 minutes ago</h4>
+                        </main>
+                        <div className={styles.text}>
+                            <div>
+                                <span>
+                                    I am so proud of all the hard work that the dragverse team it’s putting into buildin this network - I am a lucky drag queen!💋
+                                </span>
+                            </div>
+                            <div className={styles.react}>
+                                <li><Image src={Comment} alt='' width={20} height={20} /></li>
+                                <li><Image src={Transimit} alt='' width={20} height={20} /></li>
+                                <li><Image src={Heart} alt='' width={20} height={20} /></li>
+                                <li><Image src={Brower} alt='' width={20} height={20} /></li>
+                                <li><Image src={Menu} alt='' width={20} height={20} /></li>
+                            </div>
+                        </div>
 
                     </div>
                 </div>
