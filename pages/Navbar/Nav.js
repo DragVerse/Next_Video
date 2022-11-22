5c81bb3a52d01d6f6cc63d8e16cf706cc89b418e
ImArnoldDCODES <danielsarnold010@gmail.com>
2022-11-22 12:19:46 -0800

    Check this

--- pages/Navbar/Nav.js
+++ pages/Navbar/Nav.js
@@ -6,14 +6,19 @@ import notification from '../assets/Notification.png'
 import stream from '../assets/stream.jpg'
 import styles from './Nav.module.css'
 import Userdropdown from '../Feed/Userdropdown'
+import { useState } from 'react'
 
 export default function Navbar() {
-    return(
-      <div className={styles.build}>
-        <div className={styles.content}>
+  const [show, setShow] = useState(false)
+  const toggling = () => {
+    setShow(!show)
+  }
+  return (
+    <div className={styles.build}>
+      <div className={styles.content}>
         <div className={styles.navbar}>
-          <logo><Image src={logo} alt='logo' width={100} height={100}/></logo>
-          <input placeholder='Search...' className={styles.input}/>
+          <logo><Image src={logo} alt='logo' width={100} height={100} /></logo>
+          <input placeholder='Search...' className={styles.input} />
           <ul className={styles.ul}>
             <li>About</li>
             <li>Explore</li>
@@ -21,14 +26,18 @@ export default function Navbar() {
             <li>More</li>
           </ul>
           <div className={styles.social}>
-            <Image src={message} alt='send message' width={25} height={25} className={styles.img}/>
-            <Image src={notification} alt='get notification' width={26} height={25} className={styles.img}/>
-            <Image src={stream} alt='start stream' width={30} height={20} className={styles.img}/>
-            <Image src={avatar} alt='profile' width={40} height={40} className={styles.img}/>
+            <Image src={message} alt='send message' width={25} height={25} className={styles.img} />
+            <Image src={notification} alt='get notification' width={26} height={25} className={styles.img} />
+            <Image src={stream} alt='start stream' width={30} height={20} className={styles.img} />
+            <Image src={avatar} alt='profile' width={40} height={40} className={styles.img} onClick={toggling} />
           </div>
         </div>
-          <Userdropdown />
-        </div>
+        {show ? <Userdropdown /> : null }
+        <div
+          className={styles.toggle}
+          onClick={toggling}
+        ></div>
       </div>
-    ) 
-  }
+    </div>
+  )
+}
